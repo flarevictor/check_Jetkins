1f28de3e945a2728e2aafc2f8ea32902b047c1cb
flarevictor <flarevictor@gmail.com>
2018-04-06 12:09:40 +0300

    check

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -17,6 +17,7 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
   render() {
     return (
       <h1>
+        <p>Hi</p>
         <FormattedMessage {...messages.header} />
       </h1>
     );
